c467c60c7fd13ea6bfbc8c5503b1a7fbd26398eb
niciyan <nishiyamasu@gmail.com>
2018-03-13 01:08:03 +0900

    ok

--- app.py
+++ app.py
@@ -9,10 +9,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "dev-key"
 
 class MyForm(FlaskForm):
-    name = StringField('name', validators=[
-        InputRequired()])
-    number = IntegerField('number', validators=[InputRequired("you need put data."), NumberRange(1, 100)])
-    date = DateField('date', validators=[InputRequired()])
+    number = IntegerField('Size[KB]', validators=[InputRequired("you need put data."), NumberRange(1, 99999999)])
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -20,12 +17,12 @@ def index():
     result = ""
     answer = {}
     if form.validate_on_submit():
-        result = form.name.data
-        now = datetime.now()
-        flash('your name is {}. Now:{}'.format(result, now))
-        answer['now'] = now
-        answer['x1000'] = number * 1000
-        answer = ['x1000000'] = number * 1000 * 1000
+        number = form.number.data
+        # now = datetime.now().strftime("%Y年%m月%d日 %H:%m:%S")
+        # answer['now'] = now
+        # answer['number'] = number 
+        answer = create_answer(number)
+        flash('You put {}.'.format(number))
         return render_template('greet.html',
                 form=MyForm(),
                 answer=answer
@@ -38,6 +35,30 @@ def index():
 @app.route('/greet')
 def greet():
     return jsonify({"greeting":"hello"})
+
+def create_answer(number):
+    answer = {}
+    now = datetime.now().strftime("%Y年%m月%d日 %H:%m:%S")
+    answer['now'] = now
+    # answer['number'] = number 
+    answer['per_min'] = optimize_unit(number * 60)
+    answer['per_hour'] = optimize_unit(number * 60 * 60)
+    answer['per_day'] = optimize_unit(number * 60 * 60 * 24)
+    answer['per_month'] = optimize_unit(number * 60 * 60 * 24 * 30)
+    return answer
+
+def optimize_unit(number):
+    megabyte = 1000
+    gigabyte = 1000 * 1000
+    terabyte = 1000 * 1000 * 1000
+    if number >= terabyte:
+        return "{}[TB]".format(number / terabyte) 
+    if number >= gigabyte:
+        return "{}[GB]".format(number / gigabyte) 
+    if number >= megabyte:
+        return "{}[MB]".format(number / megabyte) 
+
+    return "{}[KB]".format(number)
 
 if __name__ == '__main__':
     app.config['DEBUG'] = True
