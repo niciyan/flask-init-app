dd65739069eee97f11ff8a077b06588bd14eaa27
develop30 <develop30@develop30-no-iMac.local>
2018-03-12 18:29:55 +0900

    change

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, jsonify, redirect, flash
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, SelectField, IntegerField, DateField
 from wtforms.validators import DataRequired, NumberRange, InputRequired
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -17,10 +18,18 @@ class MyForm(FlaskForm):
 def index():
     form = MyForm()
     result = ""
+    answer = {}
     if form.validate_on_submit():
         result = form.name.data
-        flash('your name is {}.'.format(result))
-        return redirect('/')
+        now = datetime.now()
+        flash('your name is {}. Now:{}'.format(result, now))
+        answer['now'] = now
+        answer['x1000'] = number * 1000
+        answer = ['x1000000'] = number * 1000 * 1000
+        return render_template('greet.html',
+                form=MyForm(),
+                answer=answer
+                )
     return render_template('greet.html',
             form=form,
             result=result
